ec8882f66962bdd59d42e636735e0a14824bbf20
Jan Prikryl <jprk@users.noreply.github.com>
2014-11-15 20:07:07 +0100

    Update to private 2014 version.

--- compare_hashtable.py
+++ compare_hashtable.py
@@ -25,15 +25,19 @@ def compute_hash ( filepath ):
     return md.digest()
     
 # Check command line parameters
-if len ( sys.argv ) < 2 or sys.argv[1] == '-h':
-    sys.exit ( 'Usage: {0} directory_name'.format ( sys.argv[0] ))
+if len ( sys.argv ) < 3 or sys.argv[1] == '-h':
+    sys.exit ( 'Usage: {0} pickle_file_name directory_name'.format ( sys.argv[0] ))
 
 if not os.path.exists ( sys.argv[1] ):
-    sys.exit ( 'ERROR: Directory `{0}` not found!'.format ( sys.argv[1] ))
+    sys.exit ( 'ERROR: Pickle file `{0}` not found!'.format ( sys.argv[1] ))
+
+if not os.path.exists ( sys.argv[2] ):
+    sys.exit ( 'ERROR: Directory `{0}` not found!'.format ( sys.argv[2] ))
 
 print "Loading hashtable data ..."
-f = open ( 'probrat.pkl', 'rb' )
+f = open ( sys.argv[1], 'rb' )
 listtable = pickle.load ( f )
+print listtable[8]
 f.close()
 
 print "Constructing hashtable and file path info ..."
@@ -42,10 +46,12 @@ origpaths = {}
 for filesize in listtable.keys() :
     hashes = defaultdict(list)
     files  = defaultdict(list)
-    for fullpath,filehash in listtable[filesize] :
+    filedata = listtable[filesize]
+    for fullpath in filedata.keys() :
         # There might be the same files with different file names
         # (usually .svn or TeX support files or different versions of
         # figures and texts)
+        filehash = filedata[fullpath][1]
         filepath, filename = os.path.split ( fullpath )
         hashes[filehash].append ( filename )
         files[filehash].append ( fullpath )
@@ -58,7 +64,7 @@ for filesize in listtable.keys() :
 # rootdir = 'D:\\Honza\\iFolder'
 # rootdir = 'D:\\K611'
 
-rootdir = sys.argv[1]
+rootdir = sys.argv[2]
 
 hashtable_keys = hashtable.keys()
 
--- create_hashtable.py
+++ create_hashtable.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import os.path
 import sys
 import hashlib
 import pickle
@@ -19,23 +20,75 @@ def compute_hash ( filepath ):
     finally:
         f.close()
     return md.digest()
+
+# Check command line parameters
+if len ( sys.argv ) < 3 or sys.argv[1] == '-h':
+    sys.exit ( 'Usage: {0} pickle_file_name root_dir_path'.format ( sys.argv[0] ))
+
+# Assign first two to be the pickle file and the root directory
+pklname = sys.argv[1]
+rootdir = sys.argv[2]
+
+# Check if the root directory exists
+if not os.path.exists ( rootdir ) :
+    sys.exit ( 'ERROR: Root directory {0} does not exist.'.format(rootdir) )
     
-rootdir = 'K:\\Probrat'
-#rootdir = 'D:\\temp'
-hashtable = defaultdict(list)
+# Check if we shall update the pickle file
+do_hash_init = True
+if os.path.exists ( pklname ) :
+    print 'Loading hashtable data from {0} ...'.format ( pklname ),
+    f = open ( pklname, 'rb' )
+    try :
+        hashtable = pickle.load ( f )
+        do_hash_init = False
+    except EOFError :
+        sys.exit ( 'ERROR: Cannot read hash table: EOF hit.' )
+    finally :
+        f.close()
+    print 'done.'
+    print 'Pruning non-existing files from the hashtable ...'
+    for filesize in list(hashtable.keys()) :
+        for fullpath in list(hashtable[filesize].keys()) :
+            if not os.path.exists ( fullpath ) :
+                del hashtable[filesize][fullpath]
+                print '-',fullpath
+    print '... done.'
+
+if do_hash_init :
+    print 'Creating new hashtable ...'
+    hashtable = defaultdict(lambda: defaultdict(tuple))
 
 for basedir, dirs, files in os.walk ( rootdir ) :
     for file in files :
         fullpath = os.path.join ( basedir, file )
-        filesize = os.path.getsize ( fullpath )
-        filehash = compute_hash ( fullpath )
-        fileinfo = ( fullpath, filehash )
-        hashtable[filesize].append(fileinfo)
-        print fullpath, filesize
-        
-print "Saving pickle data ..."
-f = open ( 'probrat.pkl', 'wb' )
+        relpath  = os.path.relpath ( fullpath, rootdir )
+        statinfo = os.stat ( fullpath )
+        # Default is to compute the hash of the file
+        do_hash = True
+        # Check for previously stored information, if any
+        fileinfo = hashtable[statinfo.st_size][relpath]
+        if fileinfo :
+            # The file hash has been already computed.
+            # Check the file modification time - if it differs from the stored
+            # one, we will recompute the hash
+            do_hash = ( fileinfo[0] == statinfo.st_mtime )
+        # If we compute new hash, we have to update the dictionary as well
+        if do_hash :
+            filehash = compute_hash ( fullpath )
+            fileinfo = ( statinfo.st_mtime, filehash )
+            hashtable[statinfo.st_size][relpath] = fileinfo
+            print '+',
+        else :
+            print ' ',
+        print fullpath, statinfo.st_size, statinfo.st_mtime
+
+# Convert the hashtable to a normal dict
+hashtable = dict(hashtable)
+
+print 'Saving pickle data to {0} ...'.format ( pklname ),
+f = open ( pklname, 'wb' )
 pickle.dump ( hashtable, f, pickle.HIGHEST_PROTOCOL )
 f.close()
+print 'done.'
 
 
